5dbe8f7d52451751bd89346fa370e9c152988532
drew-aie <159073200+drew-aie@users.noreply.github.com>
2024-10-03 11:34:20 -0500

    Add project files.

---
+++ Assets/Scripts/CannonballLauncher.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonballLauncher : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _cannonballPrefab;
+    [SerializeField]
+    private float _speed = 300;
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonDown(0))
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 direction = (ray.direction * 100 - ray.origin).normalized;
+        GameObject ball = Instantiate(_cannonballPrefab, transform.position, Quaternion.identity);
+        if (ball.TryGetComponent(out Rigidbody rb))
+        {
+            rb.AddForce(direction * _speed, ForceMode.Impulse);
+            Destroy(ball, 10);
+        }
+        else
+        {
+            Destroy(ball);
+        }
+    }
+}
